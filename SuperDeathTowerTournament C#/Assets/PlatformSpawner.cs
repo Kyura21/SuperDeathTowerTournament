933e157eead907ebd615129acb01e6aaee1ee322
Michele Lanzo <michele@steamfactory.it>
2017-08-12 21:23:46 +0200

    - Font aggiornato - Generatore piattaforme con randomizzatore speciali e variabile frequenza - Prefab piattaforme speciali (cambia solo il colore)

--- SuperDeathTowerTournament C#/Assets/PlatformSpawner.cs
+++ SuperDeathTowerTournament C#/Assets/PlatformSpawner.cs
@@ -11,13 +11,20 @@ public class PlatformSpawner : MonoBehaviour {
 
     private Transform[] board;
     public GameObject piattaforma;
+    public GameObject[] piattaformaALT;
+    public float freq;
+
     private GameObject lift;
     private Rigidbody2D rb;
 
     public bool MOVING;
     public float speed;
     public float tempo;
-    public Text conto;
+    //public Text conto;
+    public GameObject count3;
+    public GameObject count2;
+    public GameObject count1;
+    public GameObject countGO;
     public AudioClip cd_joint;
     public AudioClip roar;
     
@@ -58,25 +65,34 @@ public class PlatformSpawner : MonoBehaviour {
     IEnumerator CountDown()
     {
         yield return new WaitForSeconds(1);
-        conto.enabled = true;
-        conto.text = "3";
+        //conto.enabled = true;
+        //conto.text = "3";
+        count3.SetActive(true);
         speaker1.clip = cd_joint;
         speaker1.volume = 0.5f;
         speaker1.Play();
         
         yield return new WaitForSeconds(1);
-        conto.text = "2";
+        count3.SetActive(false);
+        //conto.text = "2";
+        count2.SetActive(true);
+
         yield return new WaitForSeconds(1);
-        conto.text = "1";
+        count2.SetActive(false);
+        //conto.text = "1";
+        count1.SetActive(true);
         lift.transform.DOShakePosition(3.0f, shaker, 50);
         speaker2.clip = roar;
         speaker2.Play();
 
         yield return new WaitForSeconds(1);
-        conto.text = "SURVIVE!";
-        conto.color = Color.red;
+        count1.SetActive(false);
+        countGO.SetActive(true);
+        //conto.text = "SURVIVE!";
+        //conto.color = Color.red;
         yield return new WaitForSeconds(2);
-        conto.enabled = false;
+        //conto.enabled = false;
+        countGO.SetActive(false);
         MOVING = true;
     }
 
@@ -97,7 +113,16 @@ public class PlatformSpawner : MonoBehaviour {
             {
                 if (i != n1 && i != n2)
                 {
-                    GameObject.Instantiate(piattaforma, board[i].position, Quaternion.identity);
+                    if (Random.value > freq)
+                    {
+                        GameObject.Instantiate(piattaforma, board[i].position, Quaternion.identity);
+                    }
+                    else
+                    {
+                        int variazione = Random.Range(0, piattaformaALT.Length);
+                        GameObject.Instantiate(piattaformaALT[variazione], board[i].position, Quaternion.identity);
+                    }
+
                 }
             }
 
