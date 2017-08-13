eb380628915a3e497d31a39b9e026d6605d99341
Michele Lanzo <michele@steamfactory.it>
2017-08-13 19:05:03 +0200

    - MiddleGround generator - Middle ground definitivo - Background spazio infinito - Aggiunto agli asset animazione barre laterali

---
+++ SuperDeathTowerTournament C#/Assets/MiddleGroundReplica.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleGroundReplica : MonoBehaviour {
+    public GameObject middleGround;
+    public Transform spawnPoint;
+    private GameObject botta;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "MiddleGround")
+        {
+            botta = collision.gameObject;
+            spawnPoint = botta.transform.Find("Spawner");
+            GameObject.Instantiate(middleGround, spawnPoint);
+            spawnPoint.DetachChildren();
+        }
+    }
+
+}
